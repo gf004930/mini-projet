791cc95c576e4a36d4728800505886f1e64c5378
root <gf004930@etu.unice.fr>
2012-12-07 12:41:42 +0100

    titre affiché en ruby et tests sur toutes les pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,16 +1,43 @@
 require 'spec_helper'
 describe "Static pages" do
+
   describe "Home page" do
-    it "should have the content 'Example App'" do
+
+    it "should have the h1 'Example App'" do
       visit '/static_pages/home'
-      page.should have_content('Example App')
+      page.should have_selector('h1', :text => 'Example App')
+    end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+          :text => "Ruby on Rails Tutorial Example App | Home")
     end
   end
-end
 
-describe "Help page" do
-  it "should have the content 'Help'" do
-    visit '/static_pages/help'
-    page.should have_content('Help')
+  describe "Help page" do
+    it "should have the h1 'Example App'" do
+      visit '/static_pages/help'
+      page.should have_selector('h1', :text => 'Example App')
+    end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('title',
+          :text => "Ruby on Rails Tutorial Example App | Help")
+    end
+  end
+
+  describe "About page" do
+    it "should have the h1 'Example App'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'Example App')
+    end
+
+    it "should have the title 'About'" do
+      visit '/static_pages/about'
+      page.should have_selector('title',
+          :text => "Ruby on Rails Tutorial Example App | About")
+    end
   end
 end
