9e024eabf452d0f3db3e07e011f8ea19cf3a4cf6
root <gf004930@etu.unice.fr>
2012-12-29 19:21:40 +0100

    tests au vert, cours 3 fini

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,41 +3,41 @@ describe "Static pages" do
 
   describe "Home page" do
 
-    it "should have the h1 'Example App'" do
+    it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'Example App')
+      page.should have_selector('h1', :text => 'Sample App')
     end
 
     it "should have the title 'Home'" do
       visit '/static_pages/home'
       page.should have_selector('title',
-          :text => "Ruby on Rails Tutorial Example App | Home")
+          :text => "Ruby on Rails Tutorial Sample App | Home")
     end
   end
 
   describe "Help page" do
-    it "should have the h1 'Example App'" do
+    it "should have the h1 'Help'" do
       visit '/static_pages/help'
-      page.should have_selector('h1', :text => 'Example App')
+      page.should have_selector('h1', :text => 'Help')
     end
 
     it "should have the title 'Help'" do
       visit '/static_pages/help'
       page.should have_selector('title',
-          :text => "Ruby on Rails Tutorial Example App | Help")
+          :text => "Ruby on Rails Tutorial Sample App | Help")
     end
   end
 
   describe "About page" do
-    it "should have the h1 'Example App'" do
+    it "should have the h1 'About Us'" do
       visit '/static_pages/about'
-      page.should have_selector('h1', :text => 'Example App')
+      page.should have_selector('h1', :text => 'About Us')
     end
 
-    it "should have the title 'About'" do
+    it "should have the title 'About Us'" do
       visit '/static_pages/about'
       page.should have_selector('title',
-          :text => "Ruby on Rails Tutorial Example App | About")
+          :text => "Ruby on Rails Tutorial Sample App | About Us")
     end
   end
 end
