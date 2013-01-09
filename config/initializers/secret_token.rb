31f82acda7ffbc42024ccfd5d29175df4212bf80
root <gf004930@etu.unice.fr>
2013-01-09 14:51:22 +0100

    micropost refinement finished

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExampleApp::Application.config.secret_token = '3066cfd9283560d1d69948628f0a02ac2a56ae7c2f28b62ffa8883ad3bfa2881654d0a067212e817967a3e10e1d4188808fa05d5e4c4164635634a16b53ea69f'
